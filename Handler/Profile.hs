7e5dd9822c9a66a158557a44b3fce1aca16ec383
Jared Magrath <magrathj@tcd.ie>
2017-02-21 18:12:25 +0000

    Display Auth Token

--- Handler/Profile.hs
+++ Handler/Profile.hs
@@ -1,17 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Handler.Profile where
 
-import Import
-import Data.List hiding(intercalate, map)
+import Import hiding (unpack, pack)
+import Data.List hiding(intercalate, map, lookup)
 import qualified GitHub.Endpoints.Repos as Github
+import GitHub.Data as GHD
+import GitHub.Data.Repos as GHDR
 import Data.Maybe
+import Data.Text.Encoding
+import Data.Text hiding(intercalate, map, lookup)
 
 getProfileR :: Handler Html
 getProfileR = do
     (_, user) <- requireAuthPair
     defaultLayout $ do
     	sess <- getSession
-    	let access_token = toList(sess) 
-    	let uname = toList(sess) 
+        let access_token = lookup "access_token" sess
+    	let uname = lookup "login" sess
     --    deets <- repoDetails uname
-        setTitle . toHtml $ (show uname)  <> "'s User page"
+        setTitle . toHtml $ (show access_token)  <> "'s User page"
         $(widgetFile "profile")
